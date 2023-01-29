f881fc7bc2b944823732ef7eae53b251e0f7c6b6
oskark22 <oskar.koskiranta@gmail.com>
2023-01-29 16:16:55 +0200

    Print elements

--- src/main/java/ie/atu/semester2/ArrayDemo.java
+++ src/main/java/ie/atu/semester2/ArrayDemo.java
@@ -1,5 +1,22 @@
 package ie.atu.semester2;
 
 public class ArrayDemo {
+    public static void main(String[] args) {
+
+        // Declares an array of integers
+        int[] anArray;
+
+        // Allocates memory for 10 integers
+        anArray = new int[10];
+
+        // Initialize the elements
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+
+        System.out.println("Element at index 0: " + anArray[0]);
+        System.out.println("Element at index 1: " + anArray[1]);
+        System.out.println("Element at index 2: " + anArray[2]);
+    }
 
 }
